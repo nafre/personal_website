c40772944834fdd2d47bc1bb7fd23a62a75d7f49
nafre <erfansiraj@yahoo.com.my>
2026-04-28 23:45:36 +0800

    Improved blog site and added a project blog

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/blog/finance-tracker/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffinance-tracker%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
 		
